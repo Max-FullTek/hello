5717ed2e9e3848feb0b5401092690b2d0c7fa6e5
Max-FullTek <max.chen@full-tek.com.tw>
2022-09-16 13:42:53 +0800

    with radome bingo

--- bingo/main.js
+++ bingo/main.js
@@ -1,3 +1,31 @@
+const fruits = [
+  '番茄',
+  '酪梨',
+  '草莓',
+  '哈密瓜',
+  '葡萄',
+  '橘子',
+  '百香果',
+  '火龍果',
+  '水蜜桃',
+  '香瓜',
+  '釋迦',
+  '蔓越莓',
+  '香蕉',
+  '水梨',
+  '芒果',
+  '桃子',
+  '蘋果',
+  '龍眼',
+  '奇異果',
+  '藍莓',
+  '柳橙',
+  '檸檬',
+  '枇杷',
+  '榴槤',
+  '鳳梨'
+];
+
 const bingo = [
   { '0': false, '1': false, '2': false, '3': false, '4': false },
   { '5': false, '6': false, '7': false, '8': false, '9': false },
@@ -17,6 +45,22 @@ document.readyState === 'loading' ? document.addEventListener('DOMContentLoaded'
 
 function ready() {
 
+  for (let i = 0; i < 5; i++) {
+    const tr = document.createElement('tr');
+    for (let j = 0; j < 5; j++) {
+      const label = document.createElement('label');
+      label.appendChild(document.createTextNode(randomPopFruits()));
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      label.appendChild(checkbox);
+      const td = document.createElement('td');
+      td.appendChild(label);
+      tr.appendChild(td);
+    }
+    document.querySelector('table').appendChild(tr);
+  }
+
+
   document.querySelectorAll('input[type="checkbox"]').forEach((checkbox, index) => {
     checkbox.addEventListener('click', function () {
       this.parentNode.parentNode.style.backgroundColor = this.checked ? 'yellow' : 'white';
@@ -33,3 +77,8 @@ function ready() {
   });
 
 }
+
+function randomPopFruits() {
+  return fruits.splice(Math.floor(Math.random() * fruits.length), 1)[0];
+}
+
